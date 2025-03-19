9bb0c0ddc4fa833dc598299535b8662a34a73db8
Jonnibb1 <jonbekollii@gmail.com>
2025-03-19 17:37:56 +0100

    ii

--- ds-theme-final-main/footer.php
+++ ds-theme-final-main/footer.php
@@ -1,7 +1,7 @@
 <footer class="site-footer">
  <div class="container">
     <div class="copyright">
-        <p>copyright Enes Nimani - All Rights Reserved</p>
+        <p>copyright Jon Bekolli - All Rights Reserved</p>
     </div>
     <nav class="footer-menu">
     <?php wp_nav_menu(
---
+++ ds-theme-final-main/general-template.php
@@ -0,0 +1,44 @@
+
+<?php
+/*
+Teamplate Name: General Translate
+*/
+?>
+
+
+
+<?php get_header();  ?>
+
+<!---->
+
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+
+            <section class="home-blog">
+                <div class="container">
+                    <div class="blog-item">
+                                <?php
+                                if(have_posts() ):
+                                    while(have_posts()) : the_post();
+                                ?>
+                                <article>
+                                    <h2><?php the_title(); ?></h2>
+                                    <?php the_content(); ?>
+                                </article>
+                                <?php endwhile; 
+                                else:?>
+                                <p>Nothing yet to be displayed!</p>
+                                <?php endif; ?>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+</div>
+<!---->
+<?php get_footer(); ?>
+
+
+
+
